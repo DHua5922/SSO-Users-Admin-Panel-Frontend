20a18155cbfeaa7a087c14398d63e31a509c34d1
DHua5922 <hua.dylan@gmail.com>
2026-08-18 13:05:16 -0500

    Add not found page

---
+++ src/app/routes/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router";
+import Button from "../../shared/components/Button";
+
+export default function NotFoundPage() {
+	const navigate = useNavigate();
+	return (
+		<div className="h-screen flex flex-col gap-8 justify-center items-center">
+			<h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+			<Button onClick={() => navigate(-1)}>Back</Button>
+		</div>
+	);
+}
