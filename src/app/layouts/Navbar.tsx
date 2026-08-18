75333bf4a653e322e2b89752ec548f21400288c9
DHua5922 <hua.dylan@gmail.com>
2026-08-17 19:54:12 -0500

    fix color contrast and focusing

--- src/app/layouts/Navbar.tsx
+++ src/app/layouts/Navbar.tsx
@@ -4,56 +4,57 @@ import logo from "../../assets/logo.svg";
 import Collapsible from "../../shared/components/Collapsible";
 import { HOME_PATH, OPEN_NAVIGATION_MENU_TEXT } from "../../shared/constants";
 import NavbarLinks from "./NavbarLinks";
+import Button from "../../shared/components/Button";
 
 interface Props extends HTMLAttributes<HTMLElement> {
-	username: string;
+  username: string;
 }
 
 const collapsibleId = "mobile-navigation";
 
 export default function Navbar({ username, className = "", ...props }: Props) {
-	const [expanded, setExpanded] = useState(false);
-	const formattedClassName = `p-6 border-b border-gray-200 ${className}`.trim();
+  const [expanded, setExpanded] = useState(false);
+  const formattedClassName = `p-6 border-b border-gray-200 ${className}`.trim();
 
-	return (
-		<nav className={formattedClassName} {...props}>
-			<div className="flex items-center justify-between">
-				<a href={HOME_PATH}>
-					<figure className="w-32">
-						<img src={logo} alt="Logo" className="max-w-full" />
-					</figure>
-				</a>
+  return (
+    <nav className={formattedClassName} {...props}>
+      <div className="flex items-center justify-between">
+        <a href={HOME_PATH}>
+          <figure className="w-32">
+            <img src={logo} alt="Logo" className="max-w-full" />
+          </figure>
+        </a>
 
-				<button
-					type="button"
-					className="md:hidden cursor-pointer bg-transparent"
-					aria-expanded={expanded}
-					aria-controls={collapsibleId}
-					onClick={() => setExpanded((prev) => !prev)}
-					aria-label={
-						expanded ? "Close navigation menu" : OPEN_NAVIGATION_MENU_TEXT
-					}
-				>
-					<Menu />
-				</button>
+        <Button
+          type="button"
+          className="md:hidden cursor-pointer bg-transparent"
+          aria-expanded={expanded}
+          aria-controls={collapsibleId}
+          onClick={() => setExpanded((prev) => !prev)}
+          aria-label={
+            expanded ? "Close navigation menu" : OPEN_NAVIGATION_MENU_TEXT
+          }
+        >
+          <Menu />
+        </Button>
 
-				<NavbarLinks
-					username={username}
-					className="hidden md:flex items-center gap-8"
-				/>
-			</div>
+        <NavbarLinks
+          username={username}
+          className="hidden md:flex items-center gap-8"
+        />
+      </div>
 
-			<Collapsible
-				expanded={expanded}
-				aria-hidden={!expanded}
-				id={collapsibleId}
-				aria-label="Mobile navigation menu"
-			>
-				<NavbarLinks
-					username={username}
-					className="py-2 flex flex-col items-center gap-6"
-				/>
-			</Collapsible>
-		</nav>
-	);
+      <Collapsible
+        expanded={expanded}
+        aria-hidden={!expanded}
+        id={collapsibleId}
+        aria-label="Mobile navigation menu"
+      >
+        <NavbarLinks
+          username={username}
+          className="py-2 flex flex-col items-center gap-6"
+        />
+      </Collapsible>
+    </nav>
+  );
 }
---
+++ src/app/layouts/NavbarLinks/index.tsx
@@ -0,0 +1,76 @@
+import { Menu as MenuComponent } from "@dhua5922/react-kit";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { ChevronDown } from "lucide-react";
+import type { HTMLAttributes } from "react";
+import { useNavigate } from "react-router";
+import { logOutApi } from "../../../features/auth/api/auth";
+import {
+  CURRENT_USER_TOGGLE_ARIA_LABEL,
+  LOGIN_PATH,
+  LOGOUT_BUTTON_TEXT,
+  ME_QUERY_KEY,
+} from "../../../features/auth/constants";
+import { ROLES_PATH } from "../../../features/roles/constants/general";
+import { USERS_PATH } from "../../../features/users/constants/general";
+import { HOME_PATH } from "../../../shared/constants";
+import usePageErrorHandler from "../../../shared/hooks/usePageErrorHandler";
+import styles from "./index.module.css";
+
+interface Props extends HTMLAttributes<HTMLUListElement> {
+  username: string;
+}
+
+function useLinks() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: logOutApi,
+    onSuccess: async () => {
+      queryClient.removeQueries({ queryKey: [ME_QUERY_KEY] });
+      navigate(LOGIN_PATH);
+    },
+    onError: usePageErrorHandler(),
+    retry: false,
+  });
+
+  return () => mutate();
+}
+
+export default function NavbarLinks({ username, ...props }: Props) {
+  const onLogOut = useLinks();
+
+  return (
+    <ul {...props}>
+      <li>
+        <a href={HOME_PATH}>Home</a>
+      </li>
+
+      <li>
+        <a href={USERS_PATH}>Users</a>
+      </li>
+
+      <li>
+        <a href={ROLES_PATH}>Roles</a>
+      </li>
+
+      <li>
+        <MenuComponent>
+          <MenuComponent.Toggle
+            className="flex items-center gap-2 cursor-pointer"
+            aria-label={CURRENT_USER_TOGGLE_ARIA_LABEL}
+          >
+            {username}
+            <ChevronDown />
+          </MenuComponent.Toggle>
+
+          <MenuComponent.Content>
+            <MenuComponent.Item className={styles["menu-item"]} onClick={onLogOut}>
+              {LOGOUT_BUTTON_TEXT}
+            </MenuComponent.Item>
+          </MenuComponent.Content>
+        </MenuComponent>
+      </li>
+    </ul>
+  );
+}
